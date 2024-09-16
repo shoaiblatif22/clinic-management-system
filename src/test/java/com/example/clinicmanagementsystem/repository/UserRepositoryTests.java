64b81fe3931901415bf47b3064af9bf05a2f9e1b
shoaib.latif <shoaiblatif@deloitte.co.uk>
2024-09-16 20:37:14 +0100

    Refactored UserRepository

--- src/test/java/com/example/clinicmanagementsystem/repository/UserRepositoryTests.java
+++ src/test/java/com/example/clinicmanagementsystem/repository/UserRepositoryTests.java
@@ -1,6 +1,43 @@
 package com.example.clinicmanagementsystem.repository;
 
+import com.example.clinicmanagementsystem.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest
 public class UserRepositoryTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @BeforeEach
+    public void setUp() {
+        UserEntity user = new UserEntity();
+        user.setFirstName("John");
+        user.setLastName("Doe");
+        userRepository.save(user);
+
+    }
+
+    @Test
+    public void whenFindByFirstName_thenReturnUser() {
+        String firstName = "John";
+        String lastName = "Doe";
+
+        UserEntity user = userRepository.findByFirstName(firstName);
+
+        assertNotNull(user);
+        assertEquals(firstName, user.getFirstName());
+        assertEquals(lastName, user.getLastName());
+
+    }
 
 
 }
