48c464a5f64cf4b3b9cefd4111d67b73d0b27def
shoaib.latif <shoaiblatif@deloitte.co.uk>
2024-09-16 20:33:06 +0100

    Refactored userrepository which should not implement userEntity correctly. Added in custom methods such as findByfirstName etc.

--- src/main/java/com/example/clinicmanagementsystem/repository/UserRepository.java
+++ src/main/java/com/example/clinicmanagementsystem/repository/UserRepository.java
@@ -4,5 +4,15 @@ package com.example.clinicmanagementsystem.repository;
 import com.example.clinicmanagementsystem.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    List<UserEntity> findAll();
+    UserEntity findById(long id);
+    UserEntity save(UserEntity user);
+    UserEntity findByFirstName(String firstName);
+//    UserEntity update(UserEntity user);
+//    void deleteById(int id);
+
 }
