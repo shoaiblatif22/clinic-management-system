cad1dd7ef385691ae9bc89d2ab2104ebb727faac
Shoaib Latif <shoaiblatif11@gmail.com>
2024-11-18 00:15:16 +0000

    Added in display name for test

--- src/test/java/com/example/clinicmanagementsystem/UserModelTests.java
+++ src/test/java/com/example/clinicmanagementsystem/UserModelTests.java
@@ -1,11 +1,13 @@
 package com.example.clinicmanagementsystem;
 
 import com.example.clinicmanagementsystem.user.model.UserModel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
 
 public class UserModelTests {
     @Test
+    @DisplayName("_1_:Test getters and setters for userModel")
     void testGettersAndSetters() {
         UserModel userModel = new UserModel();
         userModel.setFirstName("TestFN");
