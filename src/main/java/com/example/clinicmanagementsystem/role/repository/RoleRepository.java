63035acc34ee4f02aee74cfb34ed7f99c981c8c9
Shoaib Latif <shoaiblatif11@gmail.com>
2024-11-18 00:04:28 +0000

    Added in roleService to getRoleById and to connect service layer to repository layer

--- src/main/java/com/example/clinicmanagementsystem/role/repository/RoleRepository.java
+++ src/main/java/com/example/clinicmanagementsystem/role/repository/RoleRepository.java
@@ -5,6 +5,7 @@ import com.example.clinicmanagementsystem.role.Role;  // Importing the enum Role
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
-    Optional<RoleEntity> findByRoleName(Role roleName);  // RoleEntity has roleName of type Role enum
+public interface RoleRepository extends JpaRepository<RoleEntity, Integer> {
+    Optional<RoleEntity> findById(Integer roleId);
+    Optional<RoleEntity> findByRoleName(Role roleName);
 }
