006f3424b02ee2d8dae045da92cf1cb6938919a1
Shoaib Latif <shoaiblatif11@gmail.com>
2025-01-19 19:58:26 +0000

    Updated security config class by adding in value as jwkSetUri was empty

--- src/main/java/com/example/clinicmanagementsystem/webConfig/SecurityConfiguration.java
+++ src/main/java/com/example/clinicmanagementsystem/webConfig/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.example.clinicmanagementsystem.webConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,10 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
     @Bean
     public JwtDecoder customJwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri("").build();
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
     }
 
     @Bean
