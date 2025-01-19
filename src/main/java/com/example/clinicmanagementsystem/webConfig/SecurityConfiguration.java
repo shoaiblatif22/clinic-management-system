8e5a294ff50f83724c495da2add3fb4f8a246ce8
Shoaib Latif <shoaiblatif11@gmail.com>
2025-01-19 19:34:52 +0000

    Updated security configuration which disables csrf, requires authentication for all requests, configures jtw validation, sets application to be stateless and builds the security filter chain. Removed depracted code

--- src/main/java/com/example/clinicmanagementsystem/webConfig/SecurityConfiguration.java
+++ src/main/java/com/example/clinicmanagementsystem/webConfig/SecurityConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -11,7 +13,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtDecoder customJwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("").build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder customJwtDecoder) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.decoder(customJwtDecoder))
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
