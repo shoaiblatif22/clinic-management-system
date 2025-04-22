ecca45f9ffee9b15ed2b75db1f02b83a414e27d6
Shoaib Latif <shoaiblatif11@gmail.com>
2025-04-22 19:56:34 +0100

    Removed unused import

--- src/main/java/com/example/clinicmanagementsystem/user/registration/events/RegistrationCompleteEvent.java
+++ src/main/java/com/example/clinicmanagementsystem/user/registration/events/RegistrationCompleteEvent.java
@@ -1,7 +1,5 @@
 package com.example.clinicmanagementsystem.user.registration.events;
 
-import org.springframework.context.event.EventListener;
-
 import com.example.clinicmanagementsystem.user.registration.entity.ClinicAppUser;
 
 public record RegistrationCompleteEvent(ClinicAppUser clinicAppUser, String appUrl) {
