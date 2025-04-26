50174ab036eb13cc3dec720b0f6cdbcb3169726e
Shoaib Latif <shoaiblatif11@gmail.com>
2025-04-26 23:22:39 +0100

    Rehauled files

--- client/frontend/src/App.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import Router from "./components/Router.tsx";
-export function App() {
-  return <Router />;
-}
--- src/main/java/com/example/clinicmanagementsystem/user/registration/events/RegistrationCompleteEvent.java
+++
@@ -1,6 +0,0 @@
-package com.example.clinicmanagementsystem.user.registration.events;
-
-import com.example.clinicmanagementsystem.user.registration.entity.ClinicAppUser;
-
-public record RegistrationCompleteEvent(ClinicAppUser clinicAppUser, String appUrl) {
-}
